ed38e6bc09efeefe5154a7a2b3951c998525be08
Hannah Walsh <hannah.walsh@credera.com>
2022-09-09 11:30:58 -0500

    create accordion

---
+++ src/components/Accordion/Accordion.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Accordion from "./Accordion";
+
+describe("Accordion", () => {
+  test("renders the Accordion component", () => {
+    render(<Accordion title="Hello world!" content="Lorem ipsum..." />);
+  });
+});
---
+++ src/components/Accordion/Accordion.tsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import "./Accordion.scss";
+
+export interface AccordionProps {
+  title: string;
+  content: string;
+}
+
+const Accordion = (props: AccordionProps) => {
+  const [isActive, setIsActive] = useState(false);
+
+  return (
+    <div className="accordion">
+      <div className="accordion-item">
+        <div
+          className="accordion-title"
+          onClick={() => setIsActive(!isActive)}
+        >
+          <div>{props.title}</div>
+          <div className="accordion-title--icon">
+            {isActive ? '-' : '+'}
+          </div>
+        </div>
+        {isActive && <div className="accordion-content">{props.content}</div>}
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
