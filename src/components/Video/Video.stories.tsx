572a3f9625032ef112282eb9a9bbd242ac2662b7
Hannah Walsh <hannah.walsh@credera.com>
2022-10-14 15:02:24 -0500

    create the video component

---
+++ src/components/Video/Video.stories.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Video from './Video';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'React Component Library/Video',
+  component: Video,
+} as ComponentMeta<typeof Video>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof Video> = (args) => <Video {...args} />;
+
+export const VideoStory = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+VideoStory.argTypes = {
+  embedId: {
+    name: 'Embed Code ID',
+    control: { type: 'text' },
+    description: 'Embed code ID',
+  },
+  videoTitle: {
+    name: 'Video Title',
+    control: { type: 'text' },
+    description: 'Video title',
+  },
+}
+VideoStory.args = {
+  embedId: '3YrPEOu8iBU',
+  videoTitle: 'Test video',
+};
+
+VideoStory.storyName = "Video";
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from "./Button";
 export { default as BasicCard } from "./BasicCard";
 export { default as Accordion } from "./Accordion";
+export { default as Video } from "./Video";
