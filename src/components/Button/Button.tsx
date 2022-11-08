d588d532dd66055bdb7efe0b3c619a478b7af262
Hannah Walsh <hannah.walsh@credera.com>
2022-11-07 21:22:46 -0600

    test on page editor

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -7,7 +7,7 @@ export interface ButtonProps {
 }
 
 const Button = (props: ButtonProps) => {
-  return <button className={`button ${props.type}`}>{props.label}</button>;
+  return <button data-epi-edit="ButtonBlock" className={`button ${props.type}`}>{props.label}</button>;
 };
 
 export default Button;
