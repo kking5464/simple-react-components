4ab9aace3922306e989d08e9f416b33be2da201b
Adi Shankar <adi.shankar@credera.com>
2022-11-15 14:41:08 -0600

    Renamed props according to naming conventions

--- src/components/Navigation/Navigation.stories.tsx
+++ src/components/Navigation/Navigation.stories.tsx
@@ -1,119 +1,130 @@
-import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import Navigation from './Navigation';
+import Navigation from "./Navigation";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'React Component Library/Navigation/Navigation',
+  title: "React Component Library/Navigation/Navigation",
   component: Navigation,
 } as ComponentMeta<typeof Navigation>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Navigation> = (args) => <Navigation {...args} />;
+const Template: ComponentStory<typeof Navigation> = (args) => (
+  <Navigation {...args} />
+);
 
 export const NavigationStory = Template.bind({});
 NavigationStory.parameters = {
-  layout: 'fullscreen'
-}
+  layout: "fullscreen",
+};
 NavigationStory.argTypes = {
-  // Logo 
+  // Logo
   logoImage: {
-    name: 'Logo Image',
-    control: { type: 'text' },
-    description: 'Logo image',
+    name: "Logo Image",
+    control: { type: "text" },
+    description: "Logo image",
     table: {
-      category: 'Logo',
+      category: "Logo",
     },
   },
   logoLink: {
-    name: 'Logo Link',
-    control: { type: 'text' },
-    description: 'Logo link',
+    name: "Logo Link",
+    control: { type: "text" },
+    description: "Logo link",
     table: {
-      category: 'Logo',
+      category: "Logo",
     },
   },
 
   // Profile
   profileImage: {
-    name: 'Profile Image',
-    control: { type: 'text' },
-    description: 'Profile image',
+    name: "Profile Image",
+    control: { type: "text" },
+    description: "Profile image",
     table: {
-      category: 'Profile',
+      category: "Profile",
     },
   },
-  profileHome: {
+  homeLinkText: {
     name: "Profile Home",
     control: { type: "text" },
     description: "Profile home",
     table: {
       category: "Profile",
+      subcategory: "Home",
     },
   },
-  profileHomeLink: {
+  homeLinkUrl: {
     name: "Profile Home Link",
     control: { type: "text" },
     description: "Profile home link",
     table: {
-      category: "Profile Link URL",
+      category: "Profile",
+      subcategory: "Home",
     },
   },
-  profileSettings: {
+  settingsLinkText: {
     name: "Profile Settings",
     control: { type: "text" },
     description: "Profile settings",
     table: {
       category: "Profile",
+      subcategory: "Settings",
     },
   },
-  profileSettingsLink: {
+  settingsLinkUrl: {
     name: "Profile Settings Link",
     control: { type: "text" },
     description: "Profile settings link",
     table: {
-      category: "Profile Link URL",
+      category: "Profile",
+      subcategory: "Settings",
     },
   },
-  profileStore: {
+  storeName: {
     name: "Profile Store",
     control: { type: "text" },
     description: "Profile store",
     table: {
       category: "Profile",
+      subcategory: "Store",
     },
   },
-  profileStoreLinkTitle: {
+  changeStoreLinkTitle: {
     name: "Profile Store Link Title",
     control: { type: "text" },
     description: "Profile store link title",
     table: {
       category: "Profile",
+      subcategory: "Store",
     },
   },
-  profileStoreLink: {
+  changeStoreLinkUrl: {
     name: "Profile Store Link",
     control: { type: "text" },
     description: "Profile store link",
     table: {
-      category: "Profile Link URL",
+      category: "Profile",
+      subcategory: "Store",
     },
   },
-  profileLogout: {
+  logoutLinkText: {
     name: "Profile Logout",
     control: { type: "text" },
     description: "Profile logout",
     table: {
       category: "Profile",
+      subcategory: "Logout",
     },
   },
-  profileLogoutLink: {
+  logoutLinkUrl: {
     name: "Profile Logout Link",
     control: { type: "text" },
     description: "Profile logout link",
     table: {
-      category: "Profile Link URL",
+      category: "Profile",
+      subcategory: "Logout",
     },
   },
 
@@ -123,24 +134,26 @@ NavigationStory.argTypes = {
     control: { type: "object" },
     description: "Side navigation links",
     table: {
-      category: 'Side Navigation',
+      category: "Side Navigation",
     },
   },
-}
+};
 NavigationStory.args = {
-  logoImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Red_circle_frame_transparent.svg/512px-Red_circle_frame_transparent.svg.png',
-  logoLink: '/',
-  profileImage: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8aGVhZHNob3R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60',
-  profileHome: "Home",
-  profileHomeLink: "#",
-  profileSettings: "Settings",
-  profileSettingsLink: "#",
-  profileStore: "North Avenue FSU - 01885",
-  profileStoreLink: "#",
-  profileStoreLinkTitle: "Switch Locations",
-  profileLogout: "Logout",
-  profileLogoutLink: "#",
-  
+  logoImage:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/39/Red_circle_frame_transparent.svg/512px-Red_circle_frame_transparent.svg.png",
+  logoLink: "/",
+  profileImage:
+    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8aGVhZHNob3R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+  homeLinkText: "Home",
+  homeLinkUrl: "#",
+  settingsLinkText: "Settings",
+  settingsLinkUrl: "#",
+  storeName: "North Avenue FSU - 01885",
+  changeStoreLinkUrl: "#",
+  changeStoreLinkTitle: "Switch Locations",
+  logoutLinkText: "Logout",
+  logoutLinkUrl: "#",
+
   // Side Navigation
   sideNavigationLinks: [
     {
@@ -172,8 +185,8 @@ NavigationStory.args = {
       linkTitle: "Team Member Resources",
       linkUrl: "/",
       isActive: false,
-    }
-  ]
+    },
+  ],
 };
 
 NavigationStory.storyName = "Navigation";
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import RuleDivider from '../RuleDivider';
-import SideNavigation from '../SideNavigation';
+import React, { useState } from "react";
+import RuleDivider from "../RuleDivider";
+import SideNavigation from "../SideNavigation";
 import "./Navigation.scss";
 
 export interface NavigationProps {
@@ -10,15 +10,15 @@ export interface NavigationProps {
 
   // Profile
   profileImage: string;
-  profileHome: string;
-  profileHomeLink: string;
-  profileStore: string;
-  profileStoreLinkTitle: string;
-  profileStoreLink: string;
-  profileSettings: string;
-  profileSettingsLink: string;
-  profileLogout: string;
-  profileLogoutLink: string;
+  homeLinkText: string;
+  homeLinkUrl: string;
+  storeName: string;
+  changeStoreLinkTitle: string;
+  changeStoreLinkUrl: string;
+  settingsLinkText: string;
+  settingsLinkUrl: string;
+  logoutLinkText: string;
+  logoutLinkUrl: string;
 
   // Side Navigation
   sideNavigationLinks?: {
@@ -37,7 +37,11 @@ const Navigation = (props: NavigationProps) => {
       <div className="navigation">
         <div className="navigation__logo">
           <a className="navigation__logo-link" href={props.logoLink}>
-            <img className="navigation__logo-image" src={props.logoImage} alt="" />
+            <img
+              className="navigation__logo-image"
+              src={props.logoImage}
+              alt=""
+            />
           </a>
         </div>
 
@@ -47,37 +51,63 @@ const Navigation = (props: NavigationProps) => {
               className="navigation__buttons-profile-button"
               onClick={() => setIsProfileOpen(!isProfileOpen)}
             >
-              <img className="navigation__buttons-profile-image" src={props.profileImage} alt="" />
+              <img
+                className="navigation__buttons-profile-image"
+                src={props.profileImage}
+                alt=""
+              />
             </button>
-            <div className={`navigation__buttons-profile-content ${isProfileOpen && 'navigation__buttons-profile-content--open'}`}>
-              <a className="navigation__buttons-profile-content-link" href={props.profileHomeLink}>{props.profileHome}</a>
-              <a className="navigation__buttons-profile-content-link" href={props.profileSettingsLink}>{props.profileSettings}</a>
+            <div
+              className={`navigation__buttons-profile-content ${
+                isProfileOpen && "navigation__buttons-profile-content--open"
+              }`}
+            >
+              <a
+                className="navigation__buttons-profile-content-link"
+                href={props.homeLinkUrl}
+              >
+                {props.homeLinkText}
+              </a>
+              <a
+                className="navigation__buttons-profile-content-link"
+                href={props.settingsLinkUrl}
+              >
+                {props.settingsLinkText}
+              </a>
 
               <RuleDivider />
 
               <div className="navigation__buttons-profile-content-location">
                 <h3 className="navigation__buttons-profile-content-location-name">
-                  {props.profileStore}
+                  {props.storeName}
                 </h3>
-                <a className="navigation__buttons-profile-content-location-link" href={props.profileStoreLink}>
-                  {props.profileStoreLinkTitle}
+                <a
+                  className="navigation__buttons-profile-content-location-link"
+                  href={props.changeStoreLinkUrl}
+                >
+                  {props.changeStoreLinkTitle}
                 </a>
-
               </div>
 
               <RuleDivider />
 
-              <a className="navigation__buttons-profile-content-link" href={props.profileLogoutLink}>{props.profileLogout}</a>
+              <a
+                className="navigation__buttons-profile-content-link"
+                href={props.logoutLinkUrl}
+              >
+                {props.logoutLinkText}
+              </a>
             </div>
           </div>
 
           <a
-            className={`navigation__buttons-toggle ${isOpen ? 'navigation__buttons-toggle--open' : ''}`}
+            className={`navigation__buttons-toggle ${
+              isOpen ? "navigation__buttons-toggle--open" : ""
+            }`}
             onClick={() => {
               setIsOpen(!isOpen);
               setIsProfileOpen(false);
-            }
-            }
+            }}
           >
             <span className="navigation__buttons-toggle-bar"></span>
             <span className="navigation__buttons-toggle-bar"></span>
@@ -86,13 +116,19 @@ const Navigation = (props: NavigationProps) => {
         </div>
       </div>
 
-      {props.sideNavigationLinks &&
-        <SideNavigation isOpen={isOpen} sideNavigationLinks={props.sideNavigationLinks} />
-      }
+      {props.sideNavigationLinks && (
+        <SideNavigation
+          isOpen={isOpen}
+          sideNavigationLinks={props.sideNavigationLinks}
+        />
+      )}
 
-      <div className={`navigation__screen ${isOpen ? 'navigation__screen--open' : ''}`}></div>
+      <div
+        className={`navigation__screen ${
+          isOpen ? "navigation__screen--open" : ""
+        }`}
+      ></div>
     </>
-
   );
 };
 
