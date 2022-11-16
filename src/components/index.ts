1422923cf65a58529549567af57d3b6fba0d9aa7
Hannah Walsh <hannah.walsh@credera.com>
2022-11-16 13:58:54 -0600

    add icons

--- src/components/index.ts
+++ src/components/index.ts
@@ -5,6 +5,7 @@ export { default as Button } from "./Button";
 export { default as Event } from "./Event";
 export { default as FullWidthImage } from "./FullWidthImage";
 export { default as HeadingAndText } from "./HeadingAndText";
+export { default as Icon } from "./Icon";
 export { default as Navigation } from "./Navigation";
 export { default as RuleDivider } from "./RuleDivider";
 export { default as SideNavigation } from "./SideNavigation";
