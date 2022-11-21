90634ef36310e02c79fd257291f92ce7298ba13e
nicholas-phan2 <113566677+nicholas-phan2@users.noreply.github.com>
2022-11-21 14:29:06 -0600

    Update Event.test.tsx

--- src/components/Event/Event.test.tsx
+++ src/components/Event/Event.test.tsx
@@ -5,9 +5,7 @@ import Event from "./Event";
 
 describe("Event Icon", () => {
   test("renders the Event component", () => {
-    render(
-      <Event title="name" date="date" iconName="calendar" iconColor="red" />
-    );
+    render(<Event title="name" date="date" />);
   });
 });
 describe("Event Preview", () => {
@@ -24,10 +22,6 @@ describe("Event Card", () => {
         basicCardTitleColor="gray"
         date="Tuesday, November the 15th"
         basicCardDescription="In the back office at 10:00 AM"
-        basicCardButtonLabel="Button Text"
-        basicCardButtonType="secondary"
-        basicCardButtonUrl="https://www.google.com"
-        basicCardButtonColor="red"
         basicCardUseButton={true}
         image="https://images.unsplash.com/photo-1508184585643-6001cf47a1ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
       />
