eed1b2629d579a7d1f3ac31da8f4f9cf52539a64
nicholas-phan2 <113566677+nicholas-phan2@users.noreply.github.com>
2022-11-21 13:43:08 -0600

    updating test

--- src/components/Event/Event.test.tsx
+++ src/components/Event/Event.test.tsx
@@ -7,8 +7,8 @@ describe("Event Icon", () => {
   test("renders the Event component", () => {
     render(
       <Event
-        iconEventTitle="name"
-        iconEventDescription="Description"
+        title="name"
+        description="Description"
         iconName="calendar"
         iconColor="red"
       />
@@ -19,9 +19,9 @@ describe("Event Preview", () => {
   test("renders the Event component", () => {
     render(
       <Event
-        previewTitle="title"
-        previewDescription="Description"
-        previewImage="test"
+        title="title"
+        description="Description"
+        image="test"
       />
     );
   });
@@ -31,7 +31,7 @@ describe("Event Card", () => {
     render(
       <Event
         eventType="card"
-        basicCardTitle="title"
+        title="title"
         basicCardTitleColor="gray"
         basicCardSubtitle="Tuesday, November the 15th"
         basicCardCopy="In the back office at 10:00 AM"
@@ -42,8 +42,9 @@ describe("Event Card", () => {
         basicCardUseButton={true}
         basicCardCenterText={false}
         basicCardSmallTitles={false}
-        basicCardImageURL="https://images.unsplash.com/photo-1508184585643-6001cf47a1ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
+        image="https://images.unsplash.com/photo-1508184585643-6001cf47a1ca?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"
       />
     );
   });
 });
+
