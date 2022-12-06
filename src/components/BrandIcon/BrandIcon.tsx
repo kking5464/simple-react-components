6b69e145032974bebb2e5a4d17e96e07497a167d
Hannah Walsh <hannah.walsh@credera.com>
2022-12-06 12:14:54 -0600

    Add new celebration brand icon

--- src/components/BrandIcon/BrandIcon.tsx
+++ src/components/BrandIcon/BrandIcon.tsx
@@ -3,6 +3,8 @@ import "./BrandIcon.scss";
 
 import OccasionsBirthdayCakeIcon from '../../public/icons/brand-icons/occasions_birthday-cake.svg';
 import OccasionsCalendarIcon from '../../public/icons/brand-icons/occasions_calendar.svg';
+import OccastionsCelebrationIcon from '../../public/icons/brand-icons/occasions_celebration.svg';
+import OccastionsCelebration2Icon from '../../public/icons/brand-icons/occasions_celebration-2.svg';
 import OccasionsCollaborationIcon from '../../public/icons/brand-icons/occasions_collaboration.svg';
 import OccasionsDiscoIcon from '../../public/icons/brand-icons/occasions_disco.svg';
 import RestaurantTeamMemberIcon from '../../public/icons/brand-icons/restaurant_team-member.svg';
@@ -20,6 +22,10 @@ const BrandIcon = (props: BrandIconProps) => {
         return <OccasionsBirthdayCakeIcon />;
       case 'occasions_calendar':
         return <OccasionsCalendarIcon />;
+      case 'occasions_celebration':
+        return <OccastionsCelebrationIcon />;
+      case 'occasions_celebration-2':
+        return <OccastionsCelebration2Icon />;
       case 'occasions_collaboration':
         return <OccasionsCollaborationIcon />;
       case 'occasions_disco':
