dbde63dfa219123d22bb1d0e76112cc50eb58314
anna.pulis <anna.pulis@credera.com>
2022-12-13 16:42:11 -0600

    Cleaned up classes

--- src/components/PageHeader/PageHeader.tsx
+++ src/components/PageHeader/PageHeader.tsx
@@ -15,11 +15,9 @@ export interface PageHeaderProps {
 
 const PageHeader = (props: PageHeaderProps) => {
     return (
-        <div className="page-header__banner">
+        <div className="page-header">
             <Banner title={props.title} subtitle={props.subtitle} />
-            <div className="page-header__image-with-stickers" >
-                <ImageWithStickers imageSource={props.image} removeTopSticker={props.removeTopSticker} topStickerName={props.topSticker} topStickerColor={props.topStickerColor} bottomStickerName={props.bottomSticker} bottomStickerColor={props.bottomStickerColor} />
-            </div>
+            <ImageWithStickers imageSource={props.image} removeTopSticker={props.removeTopSticker} topStickerName={props.topSticker} topStickerColor={props.topStickerColor} bottomStickerName={props.bottomSticker} bottomStickerColor={props.bottomStickerColor} />
         </div>
     );
 };
