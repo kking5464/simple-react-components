a85e76e2b265628e97522f66391884a2b79b4c21
anna.pulis <anna.pulis@credera.com>
2022-12-14 15:40:24 -0600

    Nonconfigurable elements + removable list items

--- src/components/IconListItem/IconListItem.tsx
+++ src/components/IconListItem/IconListItem.tsx
@@ -4,22 +4,15 @@ import BrandIcon from "../BrandIcon";
 
 export interface IconListItemProps {
   header?: string;
-  headerColor?: string;
   subheader?: string;
-  iconName?: string;
-  iconColor?: string;
 }
 
 const IconListItem = (props: IconListItemProps) => {
   return (
   <div className="icon-list-item">
-    <BrandIcon name={props.iconName} color={props.iconColor} />
+    <BrandIcon name={"occasions_celebration-2"} color={"teal"} />
     <div className="icon-list-item__text-container">
-      {props.header ? (
-        <h3 className={`icon-list-item__header ${props.headerColor ? 'icon-list-item__header--' + props.headerColor : ''}`}>
-          {props.header}
-        </h3>
-      ):''}
+      {props.header ? <p className="icon-list-item__header">{props.header}</p> : ''}
       {props.subheader ? <p className="icon-list-item__subheader">{props.subheader}</p> : ''}
     </div>
   </div>
