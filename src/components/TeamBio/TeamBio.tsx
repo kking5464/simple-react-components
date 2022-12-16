ca184ed1bd4a65a638548bfafb67fa78b69a283b
anna.pulis <anna.pulis@credera.com>
2022-12-16 11:13:13 -0600

    Created team bio component

---
+++ src/components/TeamBio/TeamBio.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./TeamBio.scss";
+import TeamMemberProfileImage from "../TeamMemberProfileImage";
+import TextLink from "../TextLink";
+
+export interface TeamBioProps {
+  teamMemberImage: string;
+  teamMemberName: string;
+  teamMemberTitle?: string;
+  linkUrl: string;
+  linkText: string;
+}
+
+const TeamBio = (props: TeamBioProps) => {
+  return (
+    <div className="team-bio">
+      <TeamMemberProfileImage teamMemberImage={props.teamMemberImage} />
+      <p className="team-bio__team-member-name">{props.teamMemberName}</p>
+      <p className="team-bio__team-member-title">{props.teamMemberTitle}</p>
+      <TextLink linkUrl={props.linkUrl} linkText={props.linkText} />      
+    </div>
+  );
+};
+
+export default TeamBio;
